8e3993b6dacfbdef599e4585ba6d099f6d9b59bb
szp2005 <147570523+szp2005@users.noreply.github.com>
2026-04-29 09:46:02 -0700

    feat: enable @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pkm-insights.pages.dev',
-  output: 'static'
+  output: 'static',
+  integrations: [sitemap()],
 });
