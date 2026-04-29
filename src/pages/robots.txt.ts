9b25efd17a8c9b79fc4c0385c2b6b68de35a09f2
szp2005 <147570523+szp2005@users.noreply.github.com>
2026-04-29 09:57:15 -0700

    fix: robots.txt point to /sitemap.xml

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,7 +1,10 @@
-export function GET() {
-  return new Response(`User-agent: *
+import type { APIRoute } from 'astro';
+export const GET: APIRoute = () =>
+  new Response(
+    'User-agent: *
 Allow: /
 
-Sitemap: https://pkm-insights.pages.dev/sitemap-index.xml
-`);
-}
+Sitemap: https://pkm-insights.pages.dev/sitemap.xml
+',
+    { headers: { 'Content-Type': 'text/plain' } }
+  );
